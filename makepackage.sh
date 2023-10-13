d7e670557237d32823bdbecbd29dccc8fd45903d
lxq <lxq22@mails.tsinghua.edu.cn>
2023-10-13 02:37:45 +0000

    update pybinder to use general python versions.

--- makepackage.sh
+++ makepackage.sh
@@ -1,5 +1,14 @@
 echo "Making base library"
 
+# obtain default python version
+PYVER=$(python3 -c "import sys; print('{}.{}'.format(sys.version_info.major, sys.version_info.minor))")
+
+# remove the dot in the version
+PYVER_COMPACT=${PYVER//.}
+
+echo Python version is $PYVER
+echo Python version compact is $PYVER_COMPACT
+
 mkdir -p build
 cd build
 cmake ..
@@ -8,11 +17,11 @@ cd ..
 
 nvcc -x cu \
     -std=c++17 \
-    -lpython3.8 \
+    -lpython${PYVER} \
     --compiler-options -fPIC \
-    -I/usr/include/python3.8 \
+    -I/usr/include/python${PYVER} \
     -I./extern/pybind11/include/ \
-    -I./src -I/usr/include/python3.8 \
+    -I./src -I/usr/include/python${PYVER} \
     --compiler-options -fPIC \
     -c binder/binder.cu \
     -o build/binder.o
@@ -22,13 +31,13 @@ echo "Binder.o generated"
 nvcc -shared \
     ./build/src/libtroy.so \
     build/binder.o \
-    -o build/pytroy.cpython-38-x86_64-linux-gnu.so
+    -o build/pytroy.cpython-${PYVER_COMPACT}-x86_64-linux-gnu.so
     
 echo "Shared lib generated"
 
     # ./build/src/libtroy.a \
 #lib.linux-x86_64-3.8/pytroy.cpython-38-x86_64-linux-gnu.so
-cp build/pytroy.cpython-38-x86_64-linux-gnu.so ./binder/pytroy.cpython-38-x86_64-linux-gnu.so
+cp build/pytroy.cpython-${PYVER_COMPACT}-x86_64-linux-gnu.so ./binder/pytroy.cpython-${PYVER_COMPACT}-x86_64-linux-gnu.so
 cp ./build/src/libtroy.so ./binder/libtroy.so
 
 echo "Copied to ./binder"
